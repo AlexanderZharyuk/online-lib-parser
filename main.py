8ac9ebdd60a51323be6e3552c34818d7b7fdf36b
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-06-14 15:29:11 +0300

    Added parse books by ids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import requests
+from requests.exceptions import HTTPError
 
 
 def download_logo():
@@ -13,11 +14,20 @@ def download_logo():
         logo.write(response.content)
 
 
-def download_book():
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError
+
+
+def download_books():
     for book_id in range(1, 11):
         url = f'https://tululu.org/txt.php?id={book_id}'
         response = requests.get(url)
         response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except HTTPError:
+            continue
 
         os.makedirs('books', exist_ok=True)
         filename = f'id {book_id}.txt'
@@ -26,4 +36,4 @@ def download_book():
 
 
 if __name__ == '__main__':
-    download_book()
+    download_books()
