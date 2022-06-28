e1de3c4cc837065275bd1d2be9889f238b43b646
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-06-28 17:50:44 +0300

    Add file with general functions

---
+++ general_functions.py
@@ -0,0 +1,71 @@
+import os
+
+from typing import NamedTuple
+
+import requests
+
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+from requests.exceptions import HTTPError
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError
+
+
+class BookInfo(NamedTuple):
+    title: str
+    author: str
+    book_image_name: str
+    comments: list
+    genres: list
+
+
+def parse_book_page(page_html: str) -> BookInfo:
+    soup = BeautifulSoup(page_html, 'lxml')
+
+    selector = 'body h1'
+    page_info = soup.select_one(selector).text.replace(u'\xa0', '')
+    book_title, book_author = [text.strip() for text in page_info.split('::')]
+
+    selector = '.bookimage img'
+    book_image = soup.select_one(selector)['src']
+
+    selector = '.texts .black'
+    comments = [comment.text for comment in soup.select(selector)]
+
+    selector = 'span.d_book a'
+    genres = [genre.text for genre in soup.select(selector)]
+
+    return BookInfo(title=book_title, author=book_author, book_image_name=book_image,
+                    comments=comments, genres=genres)
+
+
+def download_txt(url, filename, folder='parse_results/books/'):
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    reformed_filename = f'{sanitize_filename(filename)}.txt'
+    os.makedirs(folder, exist_ok=True)
+    file_path = os.path.join(folder, reformed_filename)
+
+    with open(file_path, 'wb') as book:
+        book.write(response.text.encode("utf-8"))
+
+    return file_path
+
+
+def download_image(url, image_name, folder='parse_results/images/'):
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    os.makedirs(folder, exist_ok=True)
+    file_path = os.path.join(folder, image_name)
+
+    with open(file_path, 'wb') as image:
+        image.write(response.content)
+
+    return file_path
